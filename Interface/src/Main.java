6787e4a4d43f61e4de82f5af63ef2ddd032eb3f5
Jesus Ramirez <jesusramirez730@gmail.com>
2023-01-26 13:29:38 -0800

    created the main window, yay!

--- Interface/src/Main.java
+++ Interface/src/Main.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-import java.util.*;
-
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Scanner scan = new Scanner(System.in);
-        String resp = scan.nextLine();
-        System.out.println(resp + " is the message");
+        //Lets create an instance of the main page
+        MainFrame mainframe = new MainFrame();
+
+
+
     }
 }
---
+++ Interface/src/MainFrame.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+
+
+
+
+
+public class MainFrame implements ActionListener{
+
+    JFrame frame = new JFrame();
+
+
+    MainFrame(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
+
+
