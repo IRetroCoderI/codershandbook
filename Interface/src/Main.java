1bbf516e3ffafd6f16d86e750687fbc55feda6a9
Jesus Ramirez <jesusramirez730@gmail.com>
2023-01-26 13:16:18 -0800

    first commit

---
+++ Interface/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String resp = scan.nextLine();
+        System.out.println(resp + " is the message");
+    }
+}
