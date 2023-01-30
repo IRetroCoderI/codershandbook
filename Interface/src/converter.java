1db7badd10fe71ff3c1425235eb92b280e112fee
Jesus Ramirez <jesusramirez730@gmail.com>
2023-01-29 22:31:49 -0800

    added function to read an int every tens place, will continue tomorrow

--- Interface/src/converter.java
+++ Interface/src/converter.java
@@ -8,15 +8,39 @@ public class converter {
         //let's start with decimal to binary, then binary to decimal
         //then we can deal with decimal to hex, then hex to decimal
         //after everything is done we can deal with alphabets and stuff. for now i want to work with numbers
-        int num = 27;
+        int num = 89;
+        int n = 123456789;
 
         //give decimalToBinary a number, then return a string
 
         System.out.println(decimalToBinary(num));
+        int binary = 1011001; //this number is 89
+
+
+        int one=(n/1)%10;
+        int tens= (n/10)%10;
+        int hundred = (n/100)%10;
+/*        System.out.println(one);
+        System.out.println(tens);
+        System.out.println(hundred); */
+        String nString = "" + n;
+
+        int place = 1;
+        int decNum = 0;
+        for(int i = 0; i<nString.length(); i++){
+            System.out.println((n/place)%10);
+            place *=10;
+        }
 
     }
 
-    public static int binaryToDecimal(int binary){
+    public static int binaryToDecimal(String binary){
+        //process of binary -> decimal is as follows
+        //for length of binary num, (ex. 1101 has length of 4)
+
+        for(int i = 0; i<binary.length(); i++){
+
+        }
         return 00;
     }
     public static String decimalToBinary(int decimal){
